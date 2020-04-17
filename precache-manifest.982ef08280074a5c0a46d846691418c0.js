32501d667ed37d12040b8840c7fd19a732e8cfed
Vitalik <vitalik791@gmail.com>
2020-04-17 18:28:05 +0300

    Updates

---
+++ precache-manifest.982ef08280074a5c0a46d846691418c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabb53aa359fd8ae8c2515b2fe463f47",
+    "url": "/happy-birthday/index.html"
+  },
+  {
+    "revision": "af480d5e63342458a8cd",
+    "url": "/happy-birthday/static/css/main.ab4b1778.chunk.css"
+  },
+  {
+    "revision": "c9cb78e12698180d268c",
+    "url": "/happy-birthday/static/js/2.e6f941fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/happy-birthday/static/js/2.e6f941fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af480d5e63342458a8cd",
+    "url": "/happy-birthday/static/js/main.31b2fd9e.chunk.js"
+  },
+  {
+    "revision": "792da883508eb16bb22c",
+    "url": "/happy-birthday/static/js/runtime-main.f98aac46.js"
+  }
+]);
